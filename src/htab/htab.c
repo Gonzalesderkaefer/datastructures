6275c565e4cba4140cad9c8e1032e7bcf1f42cda
Gonzalesderkaefer <149519890+Gonzalesderkaefer@users.noreply.github.com>
2025-08-21 18:51:16 +0200

    pair_init

--- src/htab/htab.c
+++ src/htab/htab.c
@@ -1,6 +1,8 @@
 // Libraries
-#include <stdbool.h>
+#include <assert.h>
 #include <stddef.h>
+#include <stdio.h>
+#include <string.h>
 
 
 /*********************************** Public ***********************************/
@@ -46,26 +48,99 @@ typedef void (*MapFreeFn)(void *);
 
 
 
+/// This type represents functions that are used for hashing
+///
+/// Parameters:
+///   - void *: pointer to the beginning of the key
+///   - size_t: length of the key (does not have to be used)
+typedef unsigned int (*MapHashFn)(void *, size_t);
 
-/********************************** Private ***********************************/
+/***************************** Pair declarations ******************************/
+/// Pair type
+///
+/// Stores key, value and metadata in one block of memory
+typedef struct pair Pair;
+
+
+/// Initialize a pair
+///
+/// This function initializes a Pair by allocating memory for it 
+/// according to [map]'s alloc function
+///
+/// Parameters:
+///   - map: Handler to a map that was retunred by map_init();
+static Pair *pair_init(const Map *map, const void *k_ptr, const void *v_ptr);
+
+
+/***************************** Map implementation *****************************/
 struct map {
+    const MapAllocFn alloc; /* Nonnull */
+    const MapFreeFn dealloc; /* Nonnull */
+    const MapHashFn hash; /* Nonnull */
     const size_t key_len;
     const size_t val_len;
+    Pair **table; /* Nonnull */
 };
 
 
 /**************************** Pair implementation *****************************/
 
-typedef struct {
+struct pair {
     void *key;
     void *value;
     size_t key_len;
     size_t val_len;
-    bool occupied;
-} Pair;
+};
 
 
 
-static Pair *pair_init(const Map *map) {
+static Pair *pair_init(const Map *map, const void *k_ptr, const void *v_ptr) {
+    // If Map was NULL something went wrong
+    if (map == NULL) {
+        fprintf(stderr,"map was NULL in %s:%d", __FILE__, __LINE__);
+        return NULL;
+    }
+
+    // Determine size of memory to be allocated
+    const size_t alloc_sz = map->key_len + map->val_len + sizeof(Pair);
+
+    Pair *new_pair = map->alloc(alloc_sz);
+    if (map->alloc == NULL) {
+        fprintf(stderr,"Memory allocation failed in %s:%d", __FILE__, __LINE__);
+        return NULL;
+    }
+
+    // Get pointer to key
+    void *key_ptr = new_pair + 1;
+
+    // Get pointer to value
+    void *val_ptr = (char *)key_ptr + map->key_len;
+
+    // Copy key
+    memcpy(key_ptr, k_ptr, map->key_len);
+
+    // Copy value
+    memcpy(val_ptr, v_ptr, map->val_len);
+
+    // Assign values
+    new_pair->val_len = map->val_len;
+    new_pair->key_len = map->key_len;
+    new_pair->key = key_ptr;
+    new_pair->value = val_ptr;
+
+
+    return new_pair;
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
